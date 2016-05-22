5f94995a11eda466c7695c94e8ef8ccb23e92f9f
SAC-CS112-Ortiz-Oscar <oo17570@student.sac.edu>
2016-05-21 17:46:23 -0700

    Update Automobile.java

--- Automobile.java
+++ Automobile.java
@@ -2,33 +2,42 @@
 public class Automobile 
 {
 	// Member variables
-	private int 	year;
+	private String 	year;
 	private String 	make;
 	private String	model;
 	private String 	color;
 	private String 	name;
-	private int 	gears;
+	private int 	numberOfGears;
 	private int		operatingCycle;
+	private int 	currentGear;
 	private int		currentSpeed;
 	private int 	distanceTraveled;
-	
+
+	public void drive(int operatingCycle)
+	{
+		for(int count = operatingCycle; count < numberOfGears; count++)
+		{
+			distanceTraveled = distanceTraveled + (currentSpeed / 60);
+		}
+	}
 	// Constructor
-	Automobile( String model, int year)
+	Automobile( String model, int numberOfGears)
 	{
 		this.model = model;
-		this.year = year;
+		this.numberOfGears = numberOfGears;
 		
-		make = "Ford";
-		color = "Black";
-		name = "Auto";
-		gears = 5;
-		operatingCycle = 0;
-		currentSpeed = 0;
-		distanceTraveled = 0;
+		this.year 				= "2006";
+		this.make 				= "Ford";
+		this.color 				= "Black";
+		this.name 				= "Auto";
+		this.operatingCycle 	= 0;
+		this.currentGear 		= 0;
+		this.currentSpeed 		= 0;
+		this.distanceTraveled 	= 0;
 	}
 	
 	// Set methods
-	public void setYear( int year) { this.year = year; }
+	public void setYear( String year) { this.year = year; }
 	
 	public void setMake(String make) { this.make = make; }
 	
@@ -38,14 +47,14 @@ public class Automobile
 	
 	public void setName(String name) { this.name = name; }
 	
-	public void setGears( int gears) { this.gears = gears; }
+	public void setNumberOfGears(int numberOfGears) { this.numberOfGears = numberOfGears; }
 	
 	public void setOperatingCycle( int operatingCycle) { this.operatingCycle = operatingCycle; }
 	
-	public void setCurrentSpeed( int currentGear ) { currentSpeed = currentGear * 10; }
+	public void setCurrentSpeed() { this.currentSpeed = currentGear * 10; }
 	
 	// Get methods
-	public int getYear() { return year; }
+	public String getYear() { return year; }
 	
 	public String  getMake() { return make; }
 	
@@ -55,11 +64,11 @@ public class Automobile
 	
 	public String getName() { return name; }
 	
-	public int getGears() { return gears; }
+	public int getNumberOfGears() { return numberOfGears; }
 	
 	public int getOperatingCycle() { return operatingCycle; }
 	
-	public int getCurrentSpeed( int currentGear) { return currentSpeed; }
+	public int getCurrentSpeed() { return currentSpeed; }
 	
 	public int getDistanceTraveled() { return distanceTraveled; }
 }
