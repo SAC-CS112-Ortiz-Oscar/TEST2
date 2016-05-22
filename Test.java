4ae4cb3240e97783e20352600fa497413dcc2c62
SAC-CS112-Ortiz-Oscar <oo17570@student.sac.edu>
2016-05-21 21:01:56 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -12,39 +12,46 @@ public class Test
 		
 		// Construct three automobiles
 		Automobile truck = new Automobile("F150", 3);
-		Automobile van = new Automobile("Transit-350", 3);
-		Automobile sportsCar = new Automobile("Mustang", 3);
+		Automobile van = new Automobile("Transit-350", 4);
+		Automobile sportsCar = new Automobile("Mustang", 6);
+		
+		truck.drive(1);
+		van.drive(1);
+		sportsCar.drive(1);
+		
+		System.out.printf("Distance traveled by the truck is: %.2f %s.", truck.getDistanceTraveled(), "miles");
+		System.out.printf("\nDistance traveled by the van is: %.2f %s.", van.getDistanceTraveled(), "miles");
+		System.out.printf("\nDistance traveled by the sports car is: %.2f %s.\n", sportsCar.getDistanceTraveled(), "miles");
 		
 		// Get number of dices
-		System.out.print("Enter number of dices: ");
+		System.out.print("\nEnter number of dices: ");
 		int diceNumber = input.nextInt();
 		
 		// Roll dice
-		Dice newRoll = new Dice();
-		int diceRoll = newRoll.throwDice(diceNumber);
+		int diceRoll1 = Dice.throwDice(diceNumber);
+		int diceRoll2 = Dice.throwDice(diceNumber);
+		int diceRoll3 = Dice.throwDice(diceNumber);
 		
-		//Operate truck
-		int truckDistance = 0;
-		for( int gear = 0; gear < diceRoll; gear++)
-		{
-			truckDistance =+ (truck.getCurrentSpeed(gear) / 60);
-		}
+		// Drive vehicles
+		truck.drive(diceRoll1);
+		van.drive(diceRoll2);
+		sportsCar.drive(diceRoll3);
 		
-		// Operate van
-		int vanDistance = 0;
-		for( int gear = 0; gear < diceRoll; gear++)
-		{
-			vanDistance =+ (van.getCurrentSpeed(gear) / 60);
-		}
+		// Display vehicle status
+		System.out.printf("\nVehicle make: %s \nVehicle model: %s \nVehicle color: %s "
+				+ "\nDistance traveled: %.2f %s. \nNumber of gears: %d\n",
+				truck.getMake(), truck.getModel(), truck.getColor(), truck.getDistanceTraveled(),
+				"miles", truck.getNumberOfGears());
 		
-		// Operate sports car
-		int sportsCarDistance = 0;
-		for( int gear = 0; gear < diceRoll; gear++)
-		{
-			sportsCarDistance =+ (sportsCar.getCurrentSpeed(gear) / 60);
-		}
+		System.out.printf("\nVehicle make: %s \nVehicle model: %s \nVehicle color: %s "
+				+ "\nDistance traveled: %.2f %s. \nNumber of gears: %d\n",
+				van.getMake(), van.getModel(), van.getColor(), van.getDistanceTraveled(),
+				"miles", van.getNumberOfGears());
 		
-		System.out.println("The distance traveled by the truck is " + truckDistance + " miles.");
+		System.out.printf("\nVehicle make: %s \nVehicle model: %s \nVehicle color: %s "
+				+ "\nDistance traveled: %.2f %s. \nNumber of gears: %d\n",
+				sportsCar.getMake(), sportsCar.getModel(), sportsCar.getColor(), sportsCar.getDistanceTraveled(),
+				"miles", sportsCar.getNumberOfGears());
 		
 	}
 }
